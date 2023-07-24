2a8d24b45ef04f159f4002309eb82b238c427ba7
Jakob Sundstrom <jakob.sundstrom1@gmail.com>
2023-07-24 13:41:52 +0200

    generate answer when pressing equal button

--- script.js
+++ script.js
@@ -33,3 +33,40 @@ const add = function (...args) {
    };
 
 console.log(operate(1,4,"/"));
+
+function appendToInput(value) {
+    var input = document.getElementById("result");
+    input.value += value;
+};
+
+function calculateResult() {
+    const inputField = document.getElementById("result");
+    const expression = inputField.value;
+  
+    // Regular expression to split the expression into numbers and operators
+    const tokens = expression.split(/(\+|\-|\*|\/)/).filter(token => token.trim() !== '');
+  
+    let result = parseFloat(tokens[0]); // Convert the first token to a number
+    let operator = null;
+  
+    for (let i = 1; i < tokens.length; i++) {
+      if (i % 2 === 1) {
+        operator = tokens[i];
+      } else {
+        const nextNumber = parseFloat(tokens[i]);
+  
+        // Handle multiplication and division before addition and subtraction
+        if (operator === "*" || operator === "/") {
+          result = operate(result, nextNumber, operator);
+        } else {
+          // If the next operator is + or -, we update the result and operator for the next iteration
+          result = operate(result, nextNumber, operator);
+        }
+      }
+    }
+  
+    inputField.value = result;
+    console.log(result);
+  }
+  
+  
