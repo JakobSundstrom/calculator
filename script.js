e587bb93d27690dac8f296accca90dff98744d46
Jakob Sundstrom <jakob.sundstrom1@gmail.com>
2023-07-26 17:36:59 +0200

    update the code with the correct functionality

--- script.js
+++ script.js
@@ -1,103 +1,55 @@
-const add = function (...args) {
-    return args.reduce((acc, val) => acc + val, 0);
-  };
-  
-  const subtract = function (...args) {
-    return args.reduce((acc, val) => acc - val);
-  };
-  
-  const multiply = function (...args) {
-    return args.reduce((acc, val) => acc * val, 1);
-  };
-  
-  const divide = function (...args) {
-    return args.reduce((acc, val) => acc / val);
-  };
-
-  const operate = function(a,b,operator){
-    if (operator === "+"){
-        return add(a,b);
-    };
-
-    if (operator === "-"){
-        return subtract(a,b);
-    };
-
-    if (operator === "*"){
-        return multiply(a,b);
-    };
-
-    if (operator === "/"){
-        return divide(a,b);
-    };
-   };
-
-console.log(operate(1,4,"/"));
+// JavaScript (script.js)
+let resultValue = 0; // Store the intermediate result
+let currentOperator = ''; // Store the current operator
+let waitingForOperand = false; // Flag to track if we're waiting for the next operand
 
 function appendToInput(value) {
-    var input = document.getElementById("result");
-    input.value += value;
-};
+  const input = document.getElementById('result');
 
-function clearInput() {
-  var input = document.getElementById("result");
-  input.value = "";
+  if (value === '+' || value === '-' || value === '*' || value === '/') {
+    // If an operator is clicked, calculate the result and display it
+    calculateResult();
+    currentOperator = value;
+    waitingForOperand = true;
+  } else {
+    if (waitingForOperand) {
+      // If waiting for the next operand, clear the input field
+      input.value = '';
+      waitingForOperand = false;
+    }
+    input.value += value; // Append the clicked value to the input field
+  }
 }
 
 function calculateResult() {
-  const inputField = document.getElementById("result");
-  const expression = inputField.value;
+  const inputField = document.getElementById('result');
+  const inputValue = parseFloat(inputField.value) || 0; // Convert the current input value to a number or use 0 if it's not a number
 
-  // Regular expression to split the expression into numbers and operators
-  const tokens = expression.split(/(\+|\-|\*|\/)/).filter(token => token.trim() !== '');
-
-  // Create two arrays to store numbers and operators separately
-  const numbers = [];
-  const operators = [];
-
-  console.log("Tokens:", tokens);
-
-  for (const token of tokens) {
-    if (!isNaN(parseFloat(token))) {
-      numbers.push(parseFloat(token));
-      console.log("Number Stack:", numbers);
-    } else {
-      while (
-        operators.length > 0 &&
-        precedence(token) <= precedence(operators[operators.length - 1])
-      ) {
-        applyOperation(numbers, operators);
-        console.log("Number Stack:", numbers);
-      }
-      operators.push(token);
-      console.log("Operator Stack:", operators);
-    }
-  }
-
-  while (operators.length > 0) {
-    applyOperation(numbers, operators);
-    console.log("Number Stack:", numbers);
-  }
-
-  const result = numbers[0];
-  inputField.value = result;
-  console.log("Result:", result);
-}
-
-function precedence(operator) {
-  if (operator === '+' || operator === '-') {
-    return 1;
-  } else if (operator === '*' || operator === '/') {
-    return 2;
+  // Perform the calculation based on the stored operator and the current and previous values
+  if (currentOperator === '+') {
+    resultValue += inputValue;
+  } else if (currentOperator === '-') {
+    resultValue -= inputValue;
+  } else if (currentOperator === '*') {
+    resultValue *= inputValue;
+  } else if (currentOperator === '/') {
+    resultValue /= inputValue;
   } else {
-    return 0;
+    // If no operator is set, update the resultValue to the current input value
+    resultValue = inputValue;
   }
+
+  inputField.value = resultValue; // Display the updated result in the input field
+
+  // After the result is calculated and displayed, reset the operator to allow continuous calculations
+  currentOperator = '';
+  waitingForOperand = false;
 }
 
-function applyOperation(numbers, operators) {
-  const operator = operators.pop();
-  const b = numbers.pop();
-  const a = numbers.pop();
-  const result = operate(a, b, operator);
-  numbers.push(result);
+function clearInput() {
+  const input = document.getElementById('result');
+  input.value = ''; // Clear the input field
+  resultValue = 0; // Reset the stored result value
+  currentOperator = ''; // Reset the current operator
+  waitingForOperand = false; // Reset the waitingForOperand flag
 }
