4f174dce65a3fae7b01cd22127ce503f33e43b1a
Jakob Sundstrom <jakob.sundstrom1@gmail.com>
2023-07-28 10:54:31 +0200

    add percentage and plus/minus toggle functionality

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let waitingForOperand = false; // Flag to track if we're waiting for the next op
 function appendToInput(value) {
   const input = document.getElementById('result');
 
-  if (value === '+' || value === '-' || value === '*' || value === '/') {
+  if (value === '+' || value === '-' || value === '*' || value === '/' || value === '%') {
     // If an operator is clicked, calculate the result and display it
     calculateResult();
     currentOperator = value;
@@ -34,6 +34,9 @@ function calculateResult() {
     resultValue *= inputValue;
   } else if (currentOperator === '/') {
     resultValue /= inputValue;
+  } 
+  else if (currentOperator === '%') {
+    resultValue = inputValue/100;
   } else {
     // If no operator is set, update the resultValue to the current input value
     resultValue = inputValue;
@@ -52,4 +55,41 @@ function clearInput() {
   resultValue = 0; // Reset the stored result value
   currentOperator = ''; // Reset the current operator
   waitingForOperand = false; // Reset the waitingForOperand flag
+}
+
+// Function to handle keyboard input
+function handleKeyPress(event) {
+  const keyValue = event.key;
+  const input = document.getElementById('result');
+  
+  // Check if the key pressed is a number, operator, or other key
+  if (/^[0-9]$/.test(keyValue)) {
+    // If the key is a number, call the appendToInput function with the corresponding number
+    appendToInput(Number(keyValue));
+  } else if (keyValue === '+' || keyValue === '-' || keyValue === '*' || keyValue === '/') {
+    // If the key is an operator, call the appendToInput function with the corresponding operator
+    appendToInput(keyValue);
+  } else if (keyValue === '.' || keyValue === ',') {
+    // If the key is a decimal point, call the appendToInput function with a decimal point
+    appendToInput('.');
+  } else if (keyValue === 'Enter' || keyValue === '=') {
+    // If the key is the Enter key or equal sign, call the calculateResult function
+    calculateResult();
+  } else if (keyValue === 'Backspace') {
+    // If the key is the Backspace key, remove the last character from the input
+    input.value = input.value.slice(0, -1);
+  } else if (keyValue === 'c' || keyValue === 'C') {
+    // If the key is the "C" or "c" key, clear the input
+    input.value = '';
+  }
+}
+
+// Add event listener to the document to listen for keypress events
+document.addEventListener('keypress', handleKeyPress);
+
+function toggleSign() {
+  const input = document.getElementById('result');
+  const currentValue = parseFloat(input.value);
+  const newValue = -currentValue;
+  input.value = newValue.toString();
 }
